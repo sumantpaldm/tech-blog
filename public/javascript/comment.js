88f8e84e060d6eca31a9b7e3a3eabf9eb9cb5cdc
SINGH 1258 <sumantpaldm@gmail.com>
2021-11-19 22:48:04 -0500

    added files with error

---
+++ public/javascript/comment.js
@@ -0,0 +1,33 @@
+async function commentFormHandler(event) {
+  event.preventDefault();
+
+  const commentText = document
+    .querySelector('textarea[name="comment-body"]')
+    .value.trim();
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  if (commentText) {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        post_id,
+        commentText,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".comment-form")
+  .addEventListener("submit", commentFormHandler);
--- server.js
+++ server.js
@@ -1,40 +1,39 @@
-const express = require('path'),
-const session = require('express-session');
-const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const expressHandlebars = require('express-handlebars');
-const helpers = require('./utils/helpers');
-
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const expresshbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const secret = {
-    secret: 'anyone',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: "secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(secret));
 
-const handle = expressHandlebars.create({ helpers });
+const helpers = require("./utils/helpers");
 
-app.engine('handlebars', handle.engine);
-app.set('view engine', 'handlebars');
+const hbs = expresshbs.create({ helpers });
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use(require('./controllers/'));
-
+app.use(require("./controllers/"));
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
