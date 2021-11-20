698ffab2c6ef560e4434e5128ab484fcde8cca03
SINGH 1258 <sumantpaldm@gmail.com>
2021-11-19 22:03:09 -0500

    added utils

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const apiRoutes = require("./api/");
+const homeRoutes = require("./home-routes.js");
+const dashRoutes = require("./dash-routes.js");
+
+router.use("/dashboard", dashRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+
+module.exports = router;
